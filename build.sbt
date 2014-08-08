303d5ba37f9969b4295e76c3cb95f2d749b90402
Charles Hunt <charleshunt@rsnplaltm43.attlocal.net>
2014-08-08 00:57:51 -0500

    add dispatch

--- build.sbt
+++ build.sbt
@@ -2,5 +2,12 @@ name := "com/cornfluence/proteus"
 
 version := "0.1.0"
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.2"
+
+libraryDependencies ++= {
+  Seq(
+    "net.databinder.dispatch" %% "dispatch-core" % "0.11.1",
+    "org.scalatest" % "scalatest_2.11" % "2.2.1" % "test"
+  )
+}
 
--- src/main/scala/com/cornfluence/proteus/Driver.scala
+++ src/main/scala/com/cornfluence/proteus/Driver.scala
@@ -5,7 +5,9 @@ package com.cornfluence.proteus
  * Created by charlesahunt on 7/29/14.
  */
 
-class Driver(host : String = "localhost", port : String = 8529, https : Boolean = false, databaseName : String) {
+import dispatch._, Defaults._
+
+class Driver(hostMachine : String = "localhost", port : Int = 8529, https : Boolean = false, databaseName : String) {
 
 //	 val connectionTimeout
 //	 val socketTimeout
@@ -16,10 +18,25 @@ class Driver(host : String = "localhost", port : String = 8529, https : Boolean 
 //	 val staleConnectionCheck
 //	 val socketKeepAlive
 
+  val arrangoHost = if(https)host(hostMachine, port).secure else host(hostMachine,port)
+
 	val connection = None
 
 	def isConnection():Boolean = {
 		if(connection.isEmpty) false else true
 	}
 
+  def getDatabases() = {
+    val req = arrangoHost.GET
+      //.setBody("")
+      .addQueryParameter("commit", "true")
+      .addHeader("Content-type", "application/json")
+
+    val result = Http(req OK as.String).either
+    result match {
+      case Right(content)         => println("Content: " + content)
+      case Left(StatusCode(404))  => println("Not found")
+      case Left(StatusCode(code)) => println("Some other code: " + code.toString)
+    }
+  }
 }
